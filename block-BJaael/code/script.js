857435d2ca9b60a56f35bebf5209bfa9a68acc9c
Gurinder Khabra <gurinderkhabra@gmail.com>
2021-12-01 08:40:04 -0800

    Nothing

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,3 +1,7 @@
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function generateRandomColor() {
   let hexCharacters = [
     "0",
@@ -21,17 +25,46 @@ function generateRandomColor() {
   let color = "#";
 
   for (let i = 0; i < 6; i++) {
-    let randomNumber = Math.floor(Math.random() * 16);
+    let randomNumber = getRandomNumber(16);
     color = color + hexCharacters[randomNumber];
   }
   return color;
 }
-console.log(generateRandomColor());
-console.log(generateRandomColor());
-console.log(generateRandomColor());
-console.log(generateRandomColor());
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 500);
+let parentBox = document.querySelector(".boxes");
+
+// 1. Create 500 boxes
+/*
+<div class="box">
+    <h3>102</h3>
+</div>
+*/
+for (let i = 0; i < 500; i++) {
+  let div = document.createElement("div");
+  div.classList.add("box");
+  let h3 = document.createElement("h3");
+  let randomNo = getRandomNumber(500);
+  h3.innerText = randomNo;
+
+  div.append(h3);
+  parentBox.append(div);
 }
-console.log(generateRandomNumber());
+// 3. Change the background color of each box to a random color
+let allBoxes = document.querySelectorAll(".box");
+function handleMouseMove() {
+  allBoxes.forEach((box) => {
+    box.style.backgroundColor = generateRandomColor();
+    // box.children[0].innerText = getRandomNumber(500);
+    // box.querySelector("h3").innerText = getRandomNumber(500);
+  });
+}
+// 4. Change random number
+// 2. addEventListener of type mousemove
+parentBox.addEventListener("mousemove", handleMouseMove);
+
+/*
+1. Generate random number
+2. Generate random color
+3. Create 500 boxes with random color & number
+4. On parentBox, where listening to the mouse move to iterate over our code
+*/
