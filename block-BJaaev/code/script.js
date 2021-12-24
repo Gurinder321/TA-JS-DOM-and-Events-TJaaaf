b291149ff1aaa02621829126756f5965f31c1f86
Gurinder Khabra <gurinderkhabra@gmail.com>
2021-12-23 21:47:36 -0800

    Completed

--- block-BJaaev/code/script.js
+++ block-BJaaev/code/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let inputText = document.querySelector("#text");
 let root = document.querySelector("ul");
 
@@ -40,7 +39,7 @@ function handleToggle(event) {
   localStorage.setItem("allTodos", JSON.stringify(allTodos));
 }
 
-function createUI(data, rootElm) {
+function createUI(data, rootElm = root) {
   rootElm.innerHTML = "";
   data.forEach((todo, i) => {
     let li = document.createElement("li");
@@ -69,88 +68,30 @@ all.classList.add("selected");
 
 clear.addEventListener("click", () => {
   let notCompleted = allTodos.filter((todo) => !todo.isDone);
-  createUI(notCompleted);
+  allTodos = notCompleted;
+  createUI(allTodos);
+
+  localStorage.setItem("allTodos", JSON.stringify(allTodos));
 });
 inputText.addEventListener("keyup", handleInput);
 
 // The all, active, completed, clear completed buttons don't work
-=======
-let input = document.querySelector(`input[type="text"]`);
-let rootElm = document.querySelector(".todo_list");
+// When "active" button is clicked, I want to show all the todo's that are not checked (not completed)
 
-let allToDo = [
-  {
-    name: "Learn React",
-    finished: true,
-  },
-  {
-    name: "Learn Dome",
-    finished: false,
-  },
-  {
-    name: "Learn React",
-    finished: false,
-  },
-  {
-    name: "Learn Dome",
-    finished: false,
-  },
-];
-
-input.addEventListener("keyup", (event) => {
-  if (event.keyCode === 13) {
-    console.log(event.target.value);
-
-    allToDo.push({
-      name: event.target.value,
-      finshed: false,
-    });
-    createTodoList();
-  }
+active.addEventListener("click", () => {
+  let notCompleted = allTodos.filter((todo) => !todo.isDone);
+  createUI(notCompleted);
 });
 
-function deleteTodo(event) {
-  let id = event.target.dataset.id;
-  allToDo.splice(id, 1);
-  createTodoList();
-}
+// When "Completed" button is clicked, I want to show all the todo's that are checked (completed)
 
-function handleChange(event) {
-  console.log(event.target);
-  let id = event.target.id;
-  console.log(id);
-}
+completed.addEventListener("click", () => {
+  let completed = allTodos.filter((todo) => todo.isDone);
+  createUI(completed);
+});
 
-function createTodoList() {
-  //     <li>
-  //     <input class="styled-checkox" id="1" type="checkbox">
-  //     <label for="1">Checkbox</label>
-  //     <span>x</span>
-  //   </li>
+// When "All" button is clicked, I want to show all the todo's. Completed and not completed
 
-  rootElm.innerHTML = "";
-
-  allToDo.forEach((todo, i) => {
-    let li = document.createElement("li");
-    let input = document.createElement("input");
-    input.classList.add("styled-checkox");
-    input.type = "checkbox";
-    input.id = "i";
-    input.checked = todo.finished;
-
-    input.addEventListener("change", handleChange);
-    let label = document.createElement("label");
-    label.for = "i";
-    label.innerText = todo.name;
-    let span = document.createElement("span");
-    span.innerText = "x";
-    span.setAttribute("data-id", i);
-
-    span.addEventListener("click", deleteTodo);
-
-    li.append(input, label, span);
-    rootElm.append(li);
-  });
-}
-createTodoList();
->>>>>>> c2f137d028747582affe3085eb514854aa44e79a
+all.addEventListener("click", () => {
+  createUI(allTodos);
+});
